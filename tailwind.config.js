551a0622dbb58a128db0f612f2df9e947e2ef916
gegehprast <gegeh.prast@gmail.com>
2022-06-17 23:14:40 +0700

    blue-floral

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "blue-floral": "rgb(21, 38, 56, 1)"
+            }
+        },
+    },
+    plugins: [],
 }
