051e3be4484b6644f1cf7de34281622ef8b96b0f
gegehprast <gegeh.prast@gmail.com>
2022-06-18 18:13:07 +0700

    utils

---
+++ utils/utils.ts
@@ -0,0 +1,5 @@
+export const isNil = (value: any) => value === undefined || value === null
+
+export const isNull = (value: any) => value === null
+
+export const isPositiveNumber = (value: number) => value > 0
