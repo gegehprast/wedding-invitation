d86e9d1ec80093a69636147c61e2cdfabe62a3f6
gegehprast <gegeh.prast@gmail.com>
2022-06-19 15:39:36 +0700

    idk

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,38 +14,48 @@ const Home = () => {
             <BgFlowerHorizontalUp className="top-0 left-0 pointer-events-none" />
             <BgFlowerHorizontalDown className="top-0 left-0 pointer-events-none" />
             
-            <PageContainer>
-                <section className="relative w-screen h-screen bg-red-400 bg-opacity-0">
-                    <button onClick={() => setCounter(prev => prev + 1)}>Click me: </button>
-                    <span>{counter}</span>
+            <PageContainer scrollContainer={scrollContainer}>
+                <section className="relative w-screen h-screen bg-red-400 bg-opacity-30">
+                    <div className="flex items-center justify-center w-full h-full">
+                        <button onClick={() => setCounter(prev => prev + 1)}>CLICK ME 1: </button>
+                        <span className="ml-1">{counter}</span>
+                    </div>
 
 
                 </section>
 
-                <section className="relative w-screen h-screen bg-green-400 bg-opacity-0">
-                    <button onClick={() => setCounter(prev => prev + 1)}>Click me: </button>
-                    <span>{counter}</span>
+                <section className="relative w-screen h-screen bg-green-400 bg-opacity-30">
+                    <div className="flex items-center justify-center w-full h-full">
+                        <button onClick={() => setCounter(prev => prev + 1)}>CLICK ME 2: </button>
+                        <span className="ml-1">{counter}</span>
+                    </div>
 
 
                 </section>
 
-                <section className="relative w-screen h-screen bg-blue-400 bg-opacity-0">
-                    <button onClick={() => setCounter(prev => prev + 1)}>Click me: </button>
-                    <span>{counter}</span>
+                <section className="relative w-screen h-screen bg-blue-400 bg-opacity-30">
+                    <div className="flex items-center justify-center w-full h-full">
+                        <button onClick={() => setCounter(prev => prev + 1)}>CLICK ME 3: </button>
+                        <span className="ml-1">{counter}</span>
+                    </div>
 
 
                 </section>
 
-                <section className="relative w-screen h-screen bg-yellow-400 bg-opacity-0">
-                    <button onClick={() => setCounter(prev => prev + 1)}>Click me: </button>
-                    <span>{counter}</span>
+                <section className="relative w-screen h-screen bg-yellow-400 bg-opacity-30">
+                    <div className="flex items-center justify-center w-full h-full">
+                        <button onClick={() => setCounter(prev => prev + 1)}>CLICK ME 4: </button>
+                        <span className="ml-1">{counter}</span>
+                    </div>
 
 
                 </section>
 
-                <section className="relative w-screen h-screen bg-opacity-0 bg-violet-400">
-                    <button onClick={() => setCounter(prev => prev + 1)}>Click me: </button>
-                    <span>{counter}</span>
+                <section className="relative w-screen h-screen bg-opacity-30 bg-violet-400">
+                    <div className="flex items-center justify-center w-full h-full">
+                        <button onClick={() => setCounter(prev => prev + 1)}>CLICK ME 5: </button>
+                        <span className="ml-1">{counter}</span>
+                    </div>
 
 
                 </section>
