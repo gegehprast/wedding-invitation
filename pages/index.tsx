7eae3db1f7d84e7a6876702c5dc0cf15ca748625
gegehprast <gegeh.prast@gmail.com>
2022-06-24 15:53:47 +0700

    sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,33 +11,45 @@ import Map from "../components/sections/Map"
 
 const Home = () => {
     const scrollContainer = useRef<HTMLDivElement>(null)
-    
+
     return (
-        <main ref={scrollContainer} 
-            className="relative w-screen h-screen overflow-hidden text-gray-100 bg-blue-floral bg-[url(/svg/flower-tile.svg)] bg-repeat bg-contain"
-        >
-            <BgFlowerHorizontalUp className="top-0 left-0 pointer-events-none" />
-            <BgFlowerHorizontalDown className="top-0 left-0 pointer-events-none" />
-            
-            <PageContainer scrollContainer={scrollContainer}>
-                {/* main section */}
-                <Main />
+        <main className="relative flex flex-row items-center justify-center w-screen h-screen bg-white">
+            <div ref={scrollContainer} className="relative w-screen h-screen md:w-[820px] md:h-[1180px] overflow-hidden text-gray-100 bg-blue-floral bg-[url(/svg/flower-tile.svg)] bg-repeat bg-contain">
+                <BgFlowerHorizontalUp className="top-0 left-0 pointer-events-none" />
+                <BgFlowerHorizontalDown className="top-0 left-0 pointer-events-none" />
 
-                {/* bride and groom */}
-                <BrideAndGroom />
-                
-                {/* days countdown */}
-                <Countdown />
+                <PageContainer scrollContainer={scrollContainer}>
+                    {/* main section */}
+                    <section className="w-screen h-screen md:w-[820px] md:h-[1180px]">
+                        <Main />
+                    </section>
 
-                {/* map */}
-                <Map />
-                
-                {/* guest book */}
-                <GuestBook />
+                    {/* bride and groom */}
+                    <section className="w-screen h-screen md:w-[820px] md:h-[1180px]">
+                        <BrideAndGroom />
+                    </section>
 
-                {/* credits */}
-                <Credits />
-            </PageContainer>
+                    {/* days countdown */}
+                    <section className="w-screen h-screen md:w-[820px] md:h-[1180px]">
+                        <Countdown />
+                    </section>
+
+                    {/* map */}
+                    <section className="w-screen h-screen md:w-[820px] md:h-[1180px]">
+                        <Map />
+                    </section>
+
+                    {/* guest book */}
+                    <section className="w-screen h-screen md:w-[820px] md:h-[1180px]">
+                        <GuestBook />
+                    </section>
+
+                    {/* credits */}
+                    <section className="w-screen h-screen md:w-[820px] md:h-[1180px]">
+                        <Credits />
+                    </section>
+                </PageContainer>
+            </div>
         </main>
     )
 }
