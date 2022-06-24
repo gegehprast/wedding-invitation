115c0d1831bfb6633cf0b94de5c4b6836d3fbc20
gegehprast <gegeh.prast@gmail.com>
2022-06-24 15:53:40 +0700

    fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import '../styles/inter.css'
+import '../styles/lovely.css'
+import '../styles/ayuku.css'
+import '../styles/LoveConchetta.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
