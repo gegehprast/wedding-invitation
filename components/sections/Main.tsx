e14919bf9d2282427cbef10324bc83632f1b82b4
gegehprast <gegeh.prast@gmail.com>
2022-06-27 22:35:08 +0700

    improve responsiveness

--- components/sections/Main.tsx
+++ components/sections/Main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
-import flowerCircle from '../../public/svg/flower-circle.svg'
+import flowerCircle from '../../public/svg/flower-circle-final.svg'
 
 const Main = () => {
     return (
-        <div className="relative flex flex-row items-center justify-center w-full h-full bg-red-400 bg-opacity-0">
+        <div className="relative flex flex-row items-center justify-center w-full h-full bg-red-400 bg-opacity-0 md:p-20 lg:p-24">
             <Image
                 src={flowerCircle}
                 alt="Gegeh & Alma"
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,39 +13,39 @@ const Home = () => {
     const scrollContainer = useRef<HTMLDivElement>(null)
 
     return (
-        <main className="relative flex flex-row items-center justify-center w-screen h-screen bg-white">
-            <div ref={scrollContainer} className="relative w-screen h-screen md:w-[820px] md:h-[1180px] overflow-hidden text-gray-100 bg-blue-floral bg-[url(/svg/flower-tile.svg)] bg-repeat bg-contain">
+        <main className="relative flex flex-row items-center justify-center w-screen h-screen bg-gray-900">
+            <div ref={scrollContainer} className="relative w-screen h-screen max-h-screen lg:w-[520px] lg:h-auto lg:aspect-[3/4] 2xl:w-[820px] 2xl:h-[1180px] 2xl:aspect-auto overflow-hidden text-gray-100 bg-blue-floral bg-[url(/svg/flower-tile.svg)] bg-repeat bg-contain">
                 <BgFlowerHorizontalUp className="top-0 left-0 pointer-events-none" />
                 <BgFlowerHorizontalDown className="top-0 left-0 pointer-events-none" />
 
                 <PageContainer scrollContainer={scrollContainer}>
                     {/* main section */}
-                    <section className="w-screen h-screen md:w-[820px] md:h-[1180px]">
+                    <section className="w-screen h-screen max-h-screen lg:w-[520px] lg:h-auto lg:aspect-[3/4] 2xl:w-[820px] 2xl:h-[1180px] 2xl:aspect-auto">
                         <Main />
                     </section>
 
                     {/* bride and groom */}
-                    <section className="w-screen h-screen md:w-[820px] md:h-[1180px]">
+                    <section className="w-screen h-screen max-h-screen lg:w-[520px] lg:h-auto lg:aspect-[3/4] 2xl:w-[820px] 2xl:h-[1180px] 2xl:aspect-auto">
                         <BrideAndGroom />
                     </section>
 
                     {/* days countdown */}
-                    <section className="w-screen h-screen md:w-[820px] md:h-[1180px]">
+                    <section className="w-screen h-screen max-h-screen lg:w-[520px] lg:h-auto lg:aspect-[3/4] 2xl:w-[820px] 2xl:h-[1180px] 2xl:aspect-auto">
                         <Countdown />
                     </section>
 
                     {/* map */}
-                    <section className="w-screen h-screen md:w-[820px] md:h-[1180px]">
+                    <section className="w-screen h-screen max-h-screen lg:w-[520px] lg:h-auto lg:aspect-[3/4] 2xl:w-[820px] 2xl:h-[1180px] 2xl:aspect-auto">
                         <Map />
                     </section>
 
                     {/* guest book */}
-                    <section className="w-screen h-screen md:w-[820px] md:h-[1180px]">
+                    <section className="w-screen h-screen max-h-screen lg:w-[520px] lg:h-auto lg:aspect-[3/4] 2xl:w-[820px] 2xl:h-[1180px] 2xl:aspect-auto">
                         <GuestBook />
                     </section>
 
                     {/* credits */}
-                    <section className="w-screen h-screen md:w-[820px] md:h-[1180px]">
+                    <section className="w-screen h-screen max-h-screen lg:w-[520px] lg:h-auto lg:aspect-[3/4] 2xl:w-[820px] 2xl:h-[1180px] 2xl:aspect-auto">
                         <Credits />
                     </section>
                 </PageContainer>
