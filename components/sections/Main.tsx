957c738a7f5422959062e10624947619bc4c307f
gegehprast <gegeh.prast@gmail.com>
2022-07-01 06:35:26 +0700

    improve font and sizing

--- components/sections/Main.tsx
+++ components/sections/Main.tsx
@@ -4,9 +4,9 @@ import flowerCircle from '../../public/svg/flower-circle-final.svg'
 
 const Main = () => {
     return (
-        <div className="relative flex flex-col items-center justify-center w-full h-full bg-red-400 bg-opacity-0 md:p-20 lg:p-24">
+        <div className="relative flex flex-col items-center justify-center w-full h-full md:p-20 lg:p-24 font-HinaMincho">
             <div className='w-full'>
-                <h1 className='text-3xl md:text-5xl text-center font-lmroman text-[color:#E6C390] '>THE WEDDING OF</h1>
+                <h1 className='text-3xl md:text-4xl text-center text-[color:#E6C390] '>THE WEDDING OF</h1>
             </div>
 
             <Image
@@ -15,10 +15,10 @@ const Main = () => {
                 alt="Gegeh & Alma"
             />
 
-            <div className='flex flex-row items-center justify-center w-full mt-6 flex-nowrap'>
-                <h3 className='text-xl md:text-3xl text-center font-lmroman text-[color:#E6C390]'>OCT</h3>
-                <h2 className='text-5xl md:text-8xl text-center font-lmroman text-[color:#E6C390] mx-3'>31</h2>
-                <h3 className='text-xl md:text-3xl text-center font-lmroman text-[color:#E6C390]'>2022</h3>
+            <div className='flex flex-row items-center justify-center w-full mt-6 flex-nowrap text-[color:#E6C390] text-center'>
+                <h3 className='mt-2 text-xl md:text-3xl md:pt-3'>OCT</h3>
+                <h2 className='mx-3 text-5xl md:text-8xl'>31</h2>
+                <h3 className='mt-2 text-xl md:text-3xl md:pt-3'>2022</h3>
             </div>
         </div>
     )
