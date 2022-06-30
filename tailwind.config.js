e79d85d01f617b48a036541d6a73dd6a41d44b09
gegehprast <gegeh.prast@gmail.com>
2022-07-01 06:35:13 +0700

    google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,25 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                "blue-floral": "rgb(21, 38, 56)"
+                "blue-floral": "rgb(21, 38, 56)",
+                "gold": "rgb(230, 195, 144)",
+            },
+
+            fontFamily: {
+                'lovely': ['lovely'],
+                'ayuku': ['ayuku'],
+                'LoveConchetta': ['"Love Conchetta"'],
+                'lmroman': ['Lmroman'],
+                'HinaMincho': ['Hina Mincho', 'serif'],
+                'NotoSerif': ['Noto Serif', 'serif'],
+                'Peddana': ['Peddana', 'serif'],
+                'Uchen': ['Uchen', 'serif'],
+                'BebasNeue': ['Bebas Neue'],
+                'Creattion': ['Creattion'],
+                'FleurDeLeah': ['Fleur De Leah'],
+                'Inter': ['Inter', 'sans-serif'],
             }
         },
-
-        fontFamily: {
-            'lovely': ['lovely'],
-            'ayuku': ['ayuku'],
-            'LoveConchetta': ['"Love Conchetta"'],
-            'lmroman': ['Lmroman', 'serif'],
-        }
     },
     plugins: [],
 }
