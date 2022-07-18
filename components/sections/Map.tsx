8d4cf85c1fde008c6550ffe595dc7d61209c00af
gegehprast <gegeh.prast@gmail.com>
2022-07-18 17:01:50 +0700

    map done

--- components/sections/Map.tsx
+++ components/sections/Map.tsx
@@ -1,30 +1,11 @@
 import React from 'react'
-import { Wrapper, Status } from "@googlemaps/react-wrapper"
-import MapComponent from '../MapComponent'
-import MapMarker from '../MapMarker'
 
 const Map = () => {
-    const [zoom, setZoom] = React.useState(15) // initial zoom
-    const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({
-        lat: -7.903019,
-        lng: 112.619763,
-    })
-
-    const onIdle = (m: google.maps.Map) => {
-        console.log("onIdle")
-        setZoom(m.getZoom()!)
-        setCenter(m.getCenter()!.toJSON())
-    }
-
-    const render = (status: Status) => {
-        return <h1>{status}</h1>
-    }
-
     return (
-        <div className="relative flex flex-col items-center justify-center w-full h-full px-4 py-20 md:py-32 text-gold">
+        <div className="relative flex flex-col items-center justify-center w-full h-full px-4 py-20 md:px-10 md:py-32 text-gold">
             <h2 className='text-5xl text-center md:text-7xl font-Creattion'>Lokasi</h2>
 
-            <div className='w-full h-full mt-10'>
+            <div className='w-full h-full mt-2'>
                 <iframe className='w-full h-full'
                     loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"
