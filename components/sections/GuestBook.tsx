c7c00c20db29b21f6914eba7bbeb2b8a74015b59
gegehprast <gegeh.prast@gmail.com>
2022-08-07 20:19:54 +0700

    imp: guest book

--- components/sections/GuestBook.tsx
+++ components/sections/GuestBook.tsx
@@ -37,23 +37,21 @@ const GuestBook = () => {
             </div>
 
             <div className='w-full px-2 mt-8 md:px-20 laptop:px-10 2xl:px-20 md:mt-14 lg:mt-28 laptop:mt-6 2xl:mt-24 font-Inter'>
-                <div className='max-h-[42vh] md:max-h-[45vh] laptop:max-h-[43vh] 2xl:max-h-[45vh] overflow-y-scroll pl-0 pr-2 md:p-4' onWheel={handleMessageWheel} ref={messageDivRef}>
+                <div className='max-h-[42vh] md:max-h-[45vh] laptop:max-h-[43vh] 2xl:max-h-[45vh] overflow-y-scroll p-2 md:p-4' onWheel={handleMessageWheel} ref={messageDivRef}>
                     {[1, 2, 3, 4, 5].map((v, i) => (<div key={i} className='flex flex-row mb-3 md:mb-6 flex-nowrap'>
-                        <div className='w-1/12'>
-                            <div className='relative rounded-full shadow-[0_3px_15px_-7px] md:shadow-[0_3px_25px_-10px] bg-gold aspect-square shadow-[color:rgba(206,240,251,1)]'>
-                                <span className='absolute text-sm font-semibold text-gray-800 transform -translate-x-1/2 -translate-y-1/2 md:text-xl font-Inter top-1/2 left-1/2'>
-                                    AD
-                                </span>
-                            </div>
-                        </div>
-
-                        <div className='w-11/12 pl-2 md:pl-4'>
+                        <div className='w-full'>
                             <div className='py-1 px-2 md:px-3 md:py-2 rounded-md shadow-[0_3px_15px_-7px] md:shadow-[0_3px_25px_-10px] bg-gold shadow-[color:rgba(206,240,251,1)]'>
-                                <p className='text-sm font-bold text-gray-800 md:text-base'>
-                                    Almalia Deve
-                                </p>
+                                <div className='flex flex-row items-center flex-nowrap'>
+                                    <span className='text-sm font-bold text-gray-800 md:text-base'>
+                                        Almalia Deve
+                                    </span>
 
-                                <p className='mt-1 text-sm leading-snug text-gray-800 md:text-base'>
+                                    <span className='px-2 ml-2 text-xs text-white rounded-lg bg-blue-floral'>
+                                        Hadir
+                                    </span>
+                                </div>
+
+                                <p className='text-xs leading-tight text-gray-800 md:leading-snug md:text-base'>
                                     Ikut bahagia teteh cantikku, seketika teringat semua perjalanan Cinta teteh dan akhirnya menemukan Cinta yang semoga jadi pelindung buat teteh. Semoga keluarga kalian selalu dalam kebahagiaan ♥🤗
                                 </p>
                             </div>
@@ -62,27 +60,25 @@ const GuestBook = () => {
                 </div>
             </div>
 
-            <div className='w-full px-2 mt-2 md:px-20 laptop:px-10 2xl:px-20'>
-                <div className='px-2 md:px-4'>
-                    <form className='flex flex-col w-11/12 pr-2 ml-auto text-black md:pl-4' onSubmit={handleSubmitGuestBook}>
-                        <input type='text' className='w-full p-1 rounded' placeholder='Nama' />
+            <div className='w-full px-2 mt-2 md:px-20 laptop:px-10 2xl:px-20 font-Inter'>
+                <form className='flex flex-col w-full pl-2 pr-4 ml-auto text-black md:pl-4 md:pr-6' onSubmit={handleSubmitGuestBook}>
+                    <input type='text' className='w-full p-1 rounded' placeholder='Nama' />
 
-                        <textarea className='w-full p-1 mt-2 rounded' maxLength={200} placeholder='Ucapan' />
+                    <textarea className='w-full p-1 mt-2 rounded' maxLength={200} placeholder='Ucapan' />
 
-                        <select className='w-full p-1 mt-2 rounded' defaultValue={0}>
-                            <option value={0} disabled={true}>Konfirmasi Kehadiran</option>
-                            <option value={1}>Akan Hadir</option>
-                            <option value={2}>Tidak Hadir</option>
-                            <option value={3}>Telah Hadir</option>
-                        </select>
+                    <select className='w-full p-1 mt-2 rounded' defaultValue={0}>
+                        <option value={0} disabled={true}>Konfirmasi Kehadiran</option>
+                        <option value={1}>Akan Hadir</option>
+                        <option value={2}>Tidak Hadir</option>
+                        <option value={3}>Telah Hadir</option>
+                    </select>
 
-                        <div className='w-full mt-2'>
-                            <button type='submit' className='px-3 py-1 rounded-2xl bg-gold text-blue-floral hover:bg-gold2'>
-                                Kirim
-                            </button>
-                        </div>
-                    </form>
-                </div>
+                    <div className='w-full mt-2'>
+                        <button type='submit' className='px-3 py-1 rounded-2xl bg-gold text-blue-floral hover:bg-gold2'>
+                            Kirim
+                        </button>
+                    </div>
+                </form>
             </div>
         </>
     )
