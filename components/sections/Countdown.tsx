f9c0ee0097dd662ffe20570aec227c9a4e7f22ee
gegehprast <gegeh.prast@gmail.com>
2022-08-07 19:31:46 +0700

    chore: fix font size

--- components/sections/Countdown.tsx
+++ components/sections/Countdown.tsx
@@ -35,7 +35,7 @@ const Counting: React.FC<CountingProps> = ({ days, hours, minutes, seconds }) =>
             <div className='w-full mt-10 text-center md:mt-14 lg:mt-28 laptop:mt-14 2xl:mt-24'>
                 <div>
                     <h2 className='text-5xl text-center md:text-7xl laptop:text-5xl 2xl:text-7xl font-Creattion'>Akad Nikah</h2>
-                    <ul className='mt-2 text-lg leading-none md:text-2xl laptop:text-base 2xl:text-2xl md:mt-4 font-BebasNeue'>
+                    <ul className='mt-2 text-lg leading-none md:text-2xl laptop:text-lg 2xl:text-2xl md:mt-4 font-BebasNeue'>
                         <li>Senin, 31 Oktober 2022</li>
                         <li>Pukul 08:00 WIB - selesai</li>
                     </ul>
@@ -43,7 +43,7 @@ const Counting: React.FC<CountingProps> = ({ days, hours, minutes, seconds }) =>
 
                 <div className='mt-10 md:mt-14 lg:mt-28 laptop:mt-10 2xl:mt-20'>
                     <h2 className='text-5xl text-center md:text-7xl laptop:text-5xl 2xl:text-7xl font-Creattion'>Resepsi</h2>
-                    <ul className='mt-2 text-lg leading-none md:text-2xl laptop:text-base 2xl:text-2xl md:mt-4 font-BebasNeue'>
+                    <ul className='mt-2 text-lg leading-none md:text-2xl laptop:text-lg 2xl:text-2xl md:mt-4 font-BebasNeue'>
                         <li>Senin, 31 Oktober 2022</li>
                         <li>Pukul 14:00 WIB - selesai</li>
                         <li>
@@ -73,7 +73,7 @@ const Counting: React.FC<CountingProps> = ({ days, hours, minutes, seconds }) =>
 
             <div className='flex mt-6 md:mt-10'>
                 <a href='https://calendar.google.com/event?action=TEMPLATE&tmeid=MnV2azlnbWg1bDI5NjNxOTI2bjA4aTFoczIgMWhxc2ppa251aDA1Mm1xbXExcjV2dTYzYmNAZw&tmsrc=1hqsjiknuh052mqmq1r5vu63bc%40group.calendar.google.com'
-                    className='flex flex-row items-center justify-center p-2 px-3 mx-auto md:p-2 md:px-4 laptop:px-2 2xl:px-4 flex-nowrap bg-gold rounded-2xl text-blue-floral'
+                    className='flex flex-row items-center justify-center p-2 px-3 mx-auto md:p-2 md:px-4 laptop:p-1 laptop:px-2 2xl:px-4 flex-nowrap bg-gold rounded-2xl text-blue-floral'
                     target={'_blank'}
                     rel='noreferrer'
                 >
