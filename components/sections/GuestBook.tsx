68a5079e6241efa5cb88fe724db586a0d1003c4d
gegehprast <gegeh.prast@gmail.com>
2022-08-07 20:02:36 +0700

    fix: imrpove guest book

--- components/sections/GuestBook.tsx
+++ components/sections/GuestBook.tsx
@@ -13,6 +13,10 @@ const GuestBook = () => {
         },
         [],
     )
+
+    const handleSubmitGuestBook: React.FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault()
+    }
     
     useEffect(() => {
         const instance = messageDivRef.current
@@ -32,8 +36,8 @@ const GuestBook = () => {
                 <h1 className='text-2xl text-center md:text-4xl laptop:text-3xl 2xl:text-4xl font-HinaMincho'>BUKU TAMU</h1>
             </div>
 
-            <div className='w-full px-2 mt-8 md:px-20 md:mt-14 lg:mt-28 laptop:mt-14 2xl:mt-24'>
-                <div className='font-Inter max-h-[45vh] md:max-h-[35vh] overflow-y-scroll pl-0 pr-2 md:p-4' onWheel={handleMessageWheel} ref={messageDivRef}>
+            <div className='w-full px-2 mt-8 md:px-20 laptop:px-10 2xl:px-20 md:mt-14 lg:mt-28 laptop:mt-14 2xl:mt-24 font-Inter'>
+                <div className='max-h-[42vh] md:max-h-[45vh] laptop:max-h-[43vh] 2xl:max-h-[45vh] overflow-y-scroll pl-0 pr-2 md:p-4' onWheel={handleMessageWheel} ref={messageDivRef}>
                     {[1, 2, 3, 4, 5].map((v, i) => (<div key={i} className='flex flex-row mb-3 md:mb-6 flex-nowrap'>
                         <div className='w-1/12'>
                             <div className='relative rounded-full shadow-[0_3px_15px_-7px] md:shadow-[0_3px_25px_-10px] bg-gold aspect-square shadow-[color:rgba(206,240,251,1)]'>
@@ -55,6 +59,29 @@ const GuestBook = () => {
                             </div>
                         </div>
                     </div>))}
+                </div>
+            </div>
+
+            <div className='w-full px-2 mt-2 md:px-20 laptop:px-10 2xl:px-20'>
+                <div className='px-2 md:px-4'>
+                    <form className='flex flex-col w-11/12 pr-2 ml-auto text-black md:pl-4' onSubmit={handleSubmitGuestBook}>
+                        <input type='text' className='w-full p-1 rounded' placeholder='Nama' />
+
+                        <textarea className='w-full p-1 mt-2 rounded' maxLength={200} placeholder='Ucapan' />
+
+                        <select className='w-full p-1 mt-2 rounded' defaultValue={0}>
+                            <option value={0} disabled={true}>Konfirmasi Kehadiran</option>
+                            <option value={1}>Akan Hadir</option>
+                            <option value={2}>Tidak Hadir</option>
+                            <option value={3}>Telah Hadir</option>
+                        </select>
+
+                        <div className='w-full mt-2'>
+                            <button type='submit' className='px-3 py-1 rounded-2xl bg-gold text-blue-floral hover:bg-gold2'>
+                                Kirim
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
