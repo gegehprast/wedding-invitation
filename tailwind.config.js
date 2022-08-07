9f26e87adf077a0f47d92324d5411f8ef04ea290
gegehprast <gegeh.prast@gmail.com>
2022-08-07 18:04:52 +0700

    feat: laptop break points

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            laptop: {
+                raw: '(min-width: 1366px)',
+            },
+            laptop1080: {
+                raw: '(min-width: 1366px)',
+            },
+            '2xl': '1536px',
+        },
         extend: {
             colors: {
-                "blue-floral": "rgb(21, 38, 56)",
-                "gold": "rgb(230, 195, 144)",
+                'blue-floral': 'rgb(21, 38, 56)',
+                gold: 'rgb(230, 195, 144)',
             },
 
             fontFamily: {
-                'lovely': ['lovely'],
-                'ayuku': ['ayuku'],
-                'LoveConchetta': ['"Love Conchetta"'],
-                'lmroman': ['Lmroman'],
-                'HinaMincho': ['Hina Mincho', 'serif'],
-                'NotoSerif': ['Noto Serif', 'serif'],
-                'Peddana': ['Peddana', 'serif'],
-                'Uchen': ['Uchen', 'serif'],
-                'BebasNeue': ['Bebas Neue'],
-                'Creattion': ['Creattion'],
-                'FleurDeLeah': ['Fleur De Leah'],
-                'Inter': ['Inter', 'sans-serif'],
+                lovely: ['lovely'],
+                ayuku: ['ayuku'],
+                lmroman: ['Lmroman'],
+                HinaMincho: ['Hina Mincho', 'serif'],
+                NotoSerif: ['Noto Serif', 'serif'],
+                Peddana: ['Peddana', 'serif'],
+                Uchen: ['Uchen', 'serif'],
+                BebasNeue: ['Bebas Neue'],
+                Creattion: ['Creattion'],
+                FleurDeLeah: ['Fleur De Leah'],
+                Inter: ['Inter', 'sans-serif'],
             },
 
             keyframes: {
@@ -39,16 +51,16 @@ module.exports = {
                     '100%': {
                         transform: 'translateX(10%)',
                         'animation-timing-function': 'cubic-bezier(0.8,0,1,1)',
-                    }
-                }
+                    },
+                },
             },
 
             animation: {
                 drop: 'drop 5000ms linear infinite',
                 ripple: 'ripple 5000ms ease-out infinite',
                 rotation: 'rotation linear infinite',
-                'bounce-right': 'bounce-right 1s infinite'
-            }
+                'bounce-right': 'bounce-right 1s infinite',
+            },
         },
     },
     plugins: [],
