28edcd8d053a90ea72dedd481513a547109969ec
gegehprast <gegeh.prast@gmail.com>
2022-08-16 18:13:58 +0700

    more icons

---
+++ components/Icons/Annotation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Annotation = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" className="w-full h-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+        </svg>
+    )
+}
+
+export default Annotation
