aef5907b3c70ab3c011ec88559a2f29d80ac3569
gegehprast <gegeh.prast@gmail.com>
2022-08-22 12:54:33 +0700

    fade delays

--- components/sections/BrideAndGroom.tsx
+++ components/sections/BrideAndGroom.tsx
@@ -13,29 +13,35 @@ const BrideAndGroom = () => {
                     !coverModalOpen
                         ? 'opacity-100'
                         : 'opacity-0 pointer-events-none'
-                } w-full transition-opacity duration-1000 delay-1000 ease-in-out`}
+                } w-full transition-opacity duration-1000 delay-[800ms] ease-in-out`}
             >
                 <h1 className="text-2xl text-center md:text-4xl laptop:text-3xl 2xl:text-4xl font-HinaMincho">
                     MEMPELAI
                 </h1>
             </div>
 
-            <div
-                className={`${
-                    !coverModalOpen
-                        ? 'opacity-100'
-                        : 'opacity-0 pointer-events-none'
-                } w-full mt-10 text-center md:mt-14 lg:mt-28 laptop:mt-10 2xl:mt-24 transition-opacity duration-1000 delay-1000 ease-in-out`}
-            >
-                <div className="px-4 md:px-20 laptop:px-16 2xl:px-20">
+            <div className="w-full mt-10 text-center md:mt-14 lg:mt-28 laptop:mt-10 2xl:mt-24">
+                <div
+                    className={`${
+                        !coverModalOpen
+                            ? 'opacity-100'
+                            : 'opacity-0 pointer-events-none'
+                    } px-4 md:px-20 laptop:px-16 2xl:px-20 transition-opacity duration-1000 delay-[1000ms] ease-in-out`}
+                >
                     <h2 className="text-xl text-center font-Peddana md:text-3xl laptop:text-2xl 2xl:text-3xl">
-                        Assalamu’alaikum Warahmatullahi Wabarakatuh. Dengan
+                        Assalamu&apos;alaikum Warahmatullahi Wabarakatuh. Dengan
                         memohon rahmat dan ridho Allah SWT, kami bermaksud
                         menyelenggarakan acara pernikahan putra-putri kami:
                     </h2>
                 </div>
 
-                <div className="mt-12 md:mt-16 lg:mt-32 laptop:mt-14 2xl:mt-24">
+                <div
+                    className={`${
+                        !coverModalOpen
+                            ? 'opacity-100'
+                            : 'opacity-0 pointer-events-none'
+                    } mt-12 md:mt-16 lg:mt-32 laptop:mt-14 2xl:mt-24 transition-opacity duration-1000 delay-[1200ms] ease-in-out`}
+                >
                     <h2 className="text-[2.75rem] leading-none text-center md:text-7xl laptop:text-5xl 2xl:text-7xl font-AlexBrush">
                         Gigih Prastyono
                     </h2>
@@ -44,13 +50,25 @@ const BrideAndGroom = () => {
                     </ul>
                 </div>
 
-                <div className="mt-10 md:mt-14 lg:mt-28 laptop:mt-10 2xl:mt-20">
+                <div
+                    className={`${
+                        !coverModalOpen
+                            ? 'opacity-100'
+                            : 'opacity-0 pointer-events-none'
+                    } mt-10 md:mt-14 lg:mt-28 laptop:mt-10 2xl:mt-20 transition-opacity duration-1000 delay-[1400ms] ease-in-out`}
+                >
                     <h2 className="text-5xl text-center md:text-7xl laptop:text-5xl 2xl:text-7xl font-AlexBrush">
                         &
                     </h2>
                 </div>
 
-                <div className="mt-10 md:mt-14 lg:mt-28 laptop:mt-10 2xl:mt-20">
+                <div
+                    className={`${
+                        !coverModalOpen
+                            ? 'opacity-100'
+                            : 'opacity-0 pointer-events-none'
+                    } mt-10 md:mt-14 lg:mt-28 laptop:mt-10 2xl:mt-20 transition-opacity duration-1000 delay-[1600ms] ease-in-out`}
+                >
                     <h2 className="text-[2.75rem] leading-none text-center md:text-7xl laptop:text-5xl 2xl:text-7xl font-AlexBrush">
                         Almalia Deve Yanti
                     </h2>
