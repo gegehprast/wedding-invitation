dea1369cb3460bbbc2a2d4899797281eabc52782
gegehprast <gegeh.prast@gmail.com>
2022-08-22 13:55:43 +0700

    type='text/css'

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-                    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Fleur+De+Leah&family=Hina+Mincho&family=Inter:wght@300;400;500;600;700&family=Noto+Serif&family=Peddana&family=Uchen&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Fleur+De+Leah&family=Hina+Mincho&family=Inter:wght@300;400;500;600;700&family=Noto+Serif&family=Peddana&family=Uchen&display=swap" rel="stylesheet" type='text/css' />
                 </Head>
                 
                 <body>
