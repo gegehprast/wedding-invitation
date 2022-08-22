a205e23ef2dbe86cca8f77597f2d5b1c2704e8a9
gegehprast <gegeh.prast@gmail.com>
2022-08-22 11:52:48 +0700

    bride and groom

--- components/sections/BrideAndGroom.tsx
+++ components/sections/BrideAndGroom.tsx
@@ -2,9 +2,47 @@ import React from 'react'
 
 const BrideAndGroom = () => {
     return (
-        <div className="relative w-full h-full bg-green-400 bg-opacity-30">
+        <>
+            <div className="w-full">
+                <h1 className="text-2xl text-center md:text-4xl laptop:text-3xl 2xl:text-4xl font-HinaMincho">
+                    MEMPELAI
+                </h1>
+            </div>
 
-        </div>
+            <div className="w-full mt-10 text-center md:mt-14 lg:mt-28 laptop:mt-10 2xl:mt-24">
+                <div className="px-4 md:px-20 laptop:px-16 2xl:px-20">
+                    <h2 className="text-xl text-center font-Peddana md:text-3xl laptop:text-2xl 2xl:text-3xl">
+                        Assalamu’alaikum Warahmatullahi Wabarakatuh. Dengan
+                        memohon rahmat dan ridho Allah SWT, kami bermaksud
+                        menyelenggarakan acara pernikahan putra-putri kami:
+                    </h2>
+                </div>
+
+                <div className="mt-12 md:mt-16 lg:mt-32 laptop:mt-14 2xl:mt-24">
+                    <h2 className="text-5xl text-center md:text-7xl laptop:text-5xl 2xl:text-7xl font-AlexBrush">
+                        Gigih Prastyono
+                    </h2>
+                    <ul className="mt-2 text-xl leading-none md:text-3xl laptop:text-2xl 2xl:text-3xl md:mt-4 font-Peddana">
+                        <li>Putra Pertama Bapak Asis & Ibu Sulastri</li>
+                    </ul>
+                </div>
+
+                <div className="mt-10 md:mt-14 lg:mt-28 laptop:mt-10 2xl:mt-20">
+                    <h2 className="text-5xl text-center md:text-7xl laptop:text-5xl 2xl:text-7xl font-AlexBrush">
+                        &
+                    </h2>
+                </div>
+
+                <div className="mt-10 md:mt-14 lg:mt-28 laptop:mt-10 2xl:mt-20">
+                    <h2 className="text-5xl text-center md:text-7xl laptop:text-5xl 2xl:text-7xl font-AlexBrush">
+                        Almalia Deve Yanti
+                    </h2>
+                    <ul className="mt-2 text-xl leading-none md:text-3xl laptop:text-2xl 2xl:text-3xl md:mt-4 font-Peddana">
+                        <li>Putri Pertama Bapak Soleh & Ibu Dewi</li>
+                    </ul>
+                </div>
+            </div>
+        </>
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -52,14 +52,21 @@ const Home = () => {
     }, [refresh])
 
     return (
-        <main className="relative flex flex-row items-center justify-center w-screen bg-gray-900" style={{ height: `${height}px` }}>
+        <main
+            className="relative flex flex-row items-center justify-center w-screen bg-gray-900"
+            style={{ height: `${height}px` }}
+        >
             <Head>
                 <title>Undangan Pernikahan Almalia & Gigih</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
                 <link rel="shortcut icon" href="/favicon.png" />
             </Head>
 
-            <div ref={scrollContainer}
+            <div
+                ref={scrollContainer}
                 className={`${WIDTH_CLASS} relative overflow-hidden text-gray-100 bg-blue-floral`}
                 style={{ maxHeight: `${height}px` }}
             >
@@ -68,28 +75,40 @@ const Home = () => {
                 <BgFlowerHorizontalUp className="top-0 left-0 pointer-events-none" />
                 <BgFlowerHorizontalDown className="top-0 left-0 pointer-events-none" />
 
-                {(currentIndex > 0) && <div className='absolute top-0 left-0 w-full h-full'>
-                    <button type='button'
-                        className='absolute flex flex-col justify-center items-center text-[color:#E6C390] bottom-[2rem] right-[0.75rem] md:bottom-[4.25rem] md:right-[1.25rem]'
-                    >
-                        <span className="flex flex-row items-center justify-center w-10 animate-bounce-right">
-                            <span className='w-4 md:w-7'>
-                                <ArrowNR className="transform rotate-180" />
+                {currentIndex > 0 && (
+                    <div className="absolute top-0 left-0 w-full h-full">
+                        <button
+                            type="button"
+                            className="absolute flex flex-col justify-center items-center text-[color:#E6C390] bottom-[2rem] right-[0.75rem] md:bottom-[4.25rem] md:right-[1.25rem]"
+                        >
+                            <span className="flex flex-row items-center justify-center w-10 animate-bounce-right">
+                                <span className="w-4 md:w-7">
+                                    <ArrowNR className="transform rotate-180" />
+                                </span>
+
+                                <span className="w-4 md:w-7">
+                                    <ArrowNR />
+                                </span>
                             </span>
 
-                            <span className='w-4 md:w-7'>
-                                <ArrowNR />
+                            <span className="text-xs text-center md:text-sm font-Inter lg:hidden">
+                                Geser
                             </span>
-                        </span>
+                            <span className="hidden text-xs text-center md:text-sm font-Inter lg:inline-block">
+                                Gulir
+                            </span>
+                        </button>
+                    </div>
+                )}
 
-                        <span className='text-xs text-center md:text-sm font-Inter lg:hidden'>Geser</span>
-                        <span className='hidden text-xs text-center md:text-sm font-Inter lg:inline-block'>Gulir</span>
-                    </button>
-                </div>}
-
-                <PageContainer scrollContainer={scrollContainer} currentIndex={currentIndex} setCurrentIndex={setCurrentIndex}>
+                <PageContainer
+                    scrollContainer={scrollContainer}
+                    currentIndex={currentIndex}
+                    setCurrentIndex={setCurrentIndex}
+                >
                     {/* main section */}
-                    <section className={`${WIDTH_CLASS} py-10 md:py-20 laptop:py-16 2xl:py-20`}
+                    <section
+                        className={`${WIDTH_CLASS} py-10 md:py-20 laptop:py-16 2xl:py-20`}
                         style={{ maxHeight: `${height}px` }}
                     >
                         <div className="relative flex flex-col items-center justify-start w-full h-full py-5 md:py-12 laptop:py-6 2xl:py-16 text-gold">
@@ -98,14 +117,18 @@ const Home = () => {
                     </section>
 
                     {/* bride and groom */}
-                    {/* <section className="w-screen h-full lg:w-[520px] lg:h-auto lg:aspect-[3/4] xl:w-[820px] xl:h-[1180px] 2xl:aspect-auto"
+                    <section
+                        className={`${WIDTH_CLASS} py-10 md:py-20 laptop:py-16 2xl:py-20`}
                         style={{ maxHeight: `${height}px` }}
                     >
-                        <BrideAndGroom />
-                    </section> */}
+                        <div className="relative flex flex-col items-center justify-start w-full h-full py-6 md:py-20 laptop:py-6 2xl:py-16 text-gold">
+                            <BrideAndGroom />
+                        </div>
+                    </section>
 
                     {/* days countdown */}
-                    <section className={`${WIDTH_CLASS} py-10 md:py-20 laptop:py-16 2xl:py-20`}
+                    <section
+                        className={`${WIDTH_CLASS} py-10 md:py-20 laptop:py-16 2xl:py-20`}
                         style={{ maxHeight: `${height}px` }}
                     >
                         <div className="relative flex flex-col items-center justify-start w-full h-full py-6 md:py-20 laptop:py-6 2xl:py-16 text-gold">
@@ -114,7 +137,8 @@ const Home = () => {
                     </section>
 
                     {/* map */}
-                    <section className={`${WIDTH_CLASS} py-10 md:py-20 laptop:py-16 2xl:py-20`}
+                    <section
+                        className={`${WIDTH_CLASS} py-10 md:py-20 laptop:py-16 2xl:py-20`}
                         style={{ maxHeight: `${height}px` }}
                     >
                         <div className="relative flex flex-col items-center justify-start w-full h-full py-6 md:py-20 laptop:py-6 2xl:py-16 text-gold">
@@ -123,7 +147,8 @@ const Home = () => {
                     </section>
 
                     {/* guest book */}
-                    <section className={`${WIDTH_CLASS} py-10 md:py-20 laptop:py-16 2xl:py-20 relative`}
+                    <section
+                        className={`${WIDTH_CLASS} py-10 md:py-20 laptop:py-16 2xl:py-20 relative`}
                         style={{ maxHeight: `${height}px` }}
                     >
                         <div className="flex flex-col items-center justify-start w-full h-full py-6 md:py-20 laptop:py-6 2xl:py-16 text-gold">
@@ -132,7 +157,8 @@ const Home = () => {
                     </section>
 
                     {/* credits */}
-                    <section className={`${WIDTH_CLASS} py-10 md:py-20 laptop:py-16 2xl:py-20`}
+                    <section
+                        className={`${WIDTH_CLASS} py-10 md:py-20 laptop:py-16 2xl:py-20`}
                         style={{ maxHeight: `${height}px` }}
                     >
                         <div className="relative flex flex-col items-center justify-start w-full h-full py-6 md:py-20 laptop:py-6 2xl:py-16 text-gold">
