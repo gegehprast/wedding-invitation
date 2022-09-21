8daa9c7b01fa1dd25a44d36295d11dcda4547eeb
gegehprast <gegeh.prast@gmail.com>
2022-09-21 15:16:09 +0700

    close btn

--- components/modals/Amplop.tsx
+++ components/modals/Amplop.tsx
@@ -89,101 +89,148 @@ const Amplop: React.FC<RemarkProps> = ({ messageDivRef, setRemarks, open, setOpe
     }, [handleDivTouch])
 
     return (
-        <div className={`${open ? 'block' : 'hidden'} absolute top-0 left-0 w-full h-full bg-blue-floral bg-opacity-50 font-Inter`} onWheel={handleDivWheel} onClick={handleModalOverLayClick} ref={mainDivRef}>
-            <div className='absolute w-full px-2 transform -translate-x-1/2 -translate-y-1/2 md:px-20 laptop:px-10 2xl:px-20 top-1/2 left-1/2'>
-                <div className='w-full p-2 bg-white border-b rounded-t-md border-gold' onClick={handleContentClick}>
-                    <h1 className='text-lg text-center'>Amplop Online</h1>
+        <div
+            className={`${
+                open ? 'block' : 'hidden'
+            } absolute top-0 left-0 w-full h-full bg-blue-floral bg-opacity-50 font-Inter`}
+            onWheel={handleDivWheel}
+            onClick={handleModalOverLayClick}
+            ref={mainDivRef}
+        >
+            <div className="absolute w-full px-2 transform -translate-x-1/2 -translate-y-1/2 md:px-20 laptop:px-10 2xl:px-20 top-1/2 left-1/2">
+                <div
+                    className="relative w-full p-2 bg-white border-b rounded-t-md border-gold"
+                    onClick={handleContentClick}
+                >
+                    <h1 className="text-lg text-center">Amplop Online</h1>
+
+                    <button
+                        className="absolute top-1/4 right-[1rem]"
+                        onClick={() => setOpen(false)}
+                    >
+                        x
+                    </button>
                 </div>
 
-                <div className='w-full p-4 pt-3 bg-white border-gold border-t rounded-b-md md:p-6 md:pt-3 max-h-[75vh] overflow-y-auto' onClick={handleContentClick}>
-                    <form className='flex flex-col w-full text-blue-floral' onSubmit={handleSubmitGuestBook}>
-                        <div className='flex flex-col justify-start w-full mt-2'>
-                            <input type='text'
+                <div
+                    className="w-full p-4 pt-3 bg-white border-gold border-t rounded-b-md md:p-6 md:pt-3 max-h-[75vh] overflow-y-auto"
+                    onClick={handleContentClick}
+                >
+                    <form
+                        className="flex flex-col w-full text-blue-floral"
+                        onSubmit={handleSubmitGuestBook}
+                    >
+                        <div className="flex flex-col justify-start w-full mt-2">
+                            <input
+                                type="text"
                                 value={fullname}
-                                className='w-full px-2 py-1 text-sm bg-gray-100 border-2 rounded border-gold'
-                                placeholder='Nama lengkap'
+                                className="w-full px-2 py-1 text-sm bg-gray-100 border-2 rounded border-gold"
+                                placeholder="Nama lengkap"
                                 disabled={submitting}
                                 required={true}
                                 onChange={e => setFullname(e.target.value)}
                             />
-                            <label className='text-[10px] md:text-xs text-blue-floral'>Beserta alamat singkat juga boleh 🤗.</label>
+                            <label className="text-[10px] md:text-xs text-blue-floral">
+                                Beserta alamat singkat juga boleh 🤗.
+                            </label>
                         </div>
 
-                        <div className='flex flex-col justify-start w-full mt-2'>
-                            <input type='text'
+                        <div className="flex flex-col justify-start w-full mt-2">
+                            <input
+                                type="text"
                                 value={contact}
-                                className='w-full px-2 py-1 text-sm bg-gray-100 border-2 rounded border-gold'
-                                placeholder='Kontak (email atau nomor Whatsapp)'
+                                className="w-full px-2 py-1 text-sm bg-gray-100 border-2 rounded border-gold"
+                                placeholder="Kontak (email atau nomor Whatsapp)"
                                 disabled={submitting}
                                 required={true}
                                 onChange={e => setContact(e.target.value)}
                             />
-                            <label className='text-[10px] md:text-xs text-blue-floral'>Informasi kontak kamu tidak akan ditampilkan.</label>
+                            <label className="text-[10px] md:text-xs text-blue-floral">
+                                Informasi kontak kamu tidak akan ditampilkan.
+                            </label>
                         </div>
 
-                        <div className='flex flex-col justify-start w-full mt-2'>
-                            <input type='text'
+                        <div className="flex flex-col justify-start w-full mt-2">
+                            <input
+                                type="text"
                                 value={bank}
-                                className='w-full px-2 py-1 text-sm bg-gray-100 border-2 rounded border-gold'
-                                placeholder='Nomor rekening'
+                                className="w-full px-2 py-1 text-sm bg-gray-100 border-2 rounded border-gold"
+                                placeholder="Nomor rekening"
                                 disabled={submitting}
                                 required={true}
                                 onChange={e => setBank(e.target.value)}
                             />
-                            <label className='text-[10px] md:text-xs text-blue-floral'>Rekening kamu tidak akan ditampilkan.</label>
+                            <label className="text-[10px] md:text-xs text-blue-floral">
+                                Rekening kamu tidak akan ditampilkan.
+                            </label>
                         </div>
 
-                        <div className='flex flex-col justify-start w-full mt-2'>
-                            <input type='number'
+                        <div className="flex flex-col justify-start w-full mt-2">
+                            <input
+                                type="number"
                                 value={amount}
-                                className='w-full px-2 py-1 text-sm bg-gray-100 border-2 rounded border-gold'
-                                placeholder='Nominal'
+                                className="w-full px-2 py-1 text-sm bg-gray-100 border-2 rounded border-gold"
+                                placeholder="Nominal"
                                 disabled={submitting}
                                 required={true}
                                 onChange={e => setAmount(e.target.value)}
                             />
-                            <label className='text-[10px] md:text-xs text-blue-floral'>Nominal kamu tidak akan ditampilkan.</label>
+                            <label className="text-[10px] md:text-xs text-blue-floral">
+                                Nominal kamu tidak akan ditampilkan.
+                            </label>
                         </div>
-                        
-                        <div className='flex flex-col justify-start w-full mt-2'>
-                            <textarea value={message}
-                                className='w-full px-2 py-1 text-sm bg-gray-100 border-2 rounded border-gold'
+
+                        <div className="flex flex-col justify-start w-full mt-2">
+                            <textarea
+                                value={message}
+                                className="w-full px-2 py-1 text-sm bg-gray-100 border-2 rounded border-gold"
                                 maxLength={200}
                                 rows={5}
-                                placeholder='Ucapan dan doa (maksimal 200 huruf ya 😉)'
+                                placeholder="Ucapan dan doa (maksimal 200 huruf ya 😉)"
                                 disabled={submitting}
                                 onChange={e => setMessage(e.target.value)}
                             />
-                            <label className='text-[10px] md:text-xs text-blue-floral'>Ucapan yang telah dikirim tidak dapat dihapus.</label>
+                            <label className="text-[10px] md:text-xs text-blue-floral">
+                                Ucapan yang telah dikirim tidak dapat dihapus.
+                            </label>
                         </div>
-                        
-                        <div className='flex flex-col justify-start w-full mt-2'>
-                            <select className='w-full px-2 py-1 text-sm bg-gray-100 border-2 rounded border-gold'
+
+                        <div className="flex flex-col justify-start w-full mt-2">
+                            <select
+                                className="w-full px-2 py-1 text-sm bg-gray-100 border-2 rounded border-gold"
                                 value={presence}
                                 disabled={submitting}
                                 required={true}
-                                onChange={(e) => setPresence(parseInt(e.target.value))}
+                                onChange={e =>
+                                    setPresence(parseInt(e.target.value))
+                                }
                             >
-                                <option value={0} disabled={true} >Konfirmasi Kehadiran</option>
+                                <option
+                                    value={0}
+                                    disabled={true}
+                                >
+                                    Konfirmasi Kehadiran
+                                </option>
                                 <option value={1}>Akan Hadir</option>
                                 <option value={2}>Tidak Hadir</option>
                                 <option value={3}>Telah Hadir</option>
                             </select>
                         </div>
 
-                        <div className='flex flex-col justify-start w-full mt-4'>
-                            <label className='text-sm text-blue-floral'>
-                                Silakan transfer ke rekening BCA atau scan kode QRIS di bawah ini.
+                        <div className="flex flex-col justify-start w-full mt-4">
+                            <label className="text-sm text-blue-floral">
+                                Silakan transfer ke rekening BCA atau scan kode
+                                QRIS di bawah ini.
                             </label>
 
-                            <label className='mt-3 text-sm font-semibold text-center text-blue-floral'>
+                            <label className="mt-3 text-sm font-semibold text-center text-blue-floral">
                                 BCA 3160114281 - GIGIH PRASTYONO
                             </label>
 
-                            <div className='md:px-40'>
-                                <div className='border-2 rounded border-gold'>
+                            <div className="md:px-40">
+                                <div className="border-2 rounded border-gold">
                                     <Image
-                                        className='mb-6 pointer-events-none'
+                                        className="mb-6 pointer-events-none"
                                         src={qrisbca}
                                         alt="QRIS BCA"
                                     />
@@ -191,8 +238,11 @@ const Amplop: React.FC<RemarkProps> = ({ messageDivRef, setRemarks, open, setOpe
                             </div>
                         </div>
 
-                        <div className='flex flex-row flex-wrap items-center w-full mt-4'>
-                            <button type='submit' className='w-full px-3 py-1 text-sm rounded-2xl bg-gold text-blue-floral hover:bg-gold2'>
+                        <div className="flex flex-row flex-wrap items-center w-full mt-4">
+                            <button
+                                type="submit"
+                                className="w-full px-3 py-1 text-sm rounded-2xl bg-gold text-blue-floral hover:bg-gold2"
+                            >
                                 {submitting ? 'Mengirim...' : 'Kirim'}
                             </button>
                         </div>
--- components/modals/Remark.tsx
+++ components/modals/Remark.tsx
@@ -84,51 +84,88 @@ const Remark: React.FC<RemarkProps> = ({ messageDivRef, setRemarks, open, setOpe
     }, [handleDivTouch])
 
     return (
-        <div className={`${open ? 'block' : 'hidden'} absolute top-0 left-0 w-full h-full bg-blue-floral bg-opacity-50 font-Inter`} onWheel={handleDivWheel} onClick={handleModalOverLayClick} ref={mainDivRef}>
-            <div className='absolute w-full px-2 transform -translate-x-1/2 -translate-y-1/2 md:px-20 laptop:px-10 2xl:px-20 top-1/2 left-1/2'>
-                <div className='w-full p-2 bg-white border-b rounded-t-md border-gold' onClick={handleContentClick}>
-                    <h1 className='text-lg text-center'>Ucapan dan Doa</h1>
+        <div
+            className={`${
+                open ? 'block' : 'hidden'
+            } absolute top-0 left-0 w-full h-full bg-blue-floral bg-opacity-50 font-Inter`}
+            onWheel={handleDivWheel}
+            onClick={handleModalOverLayClick}
+            ref={mainDivRef}
+        >
+            <div className="absolute w-full px-2 transform -translate-x-1/2 -translate-y-1/2 md:px-20 laptop:px-10 2xl:px-20 top-1/2 left-1/2">
+                <div
+                    className="relative w-full p-2 bg-white border-b rounded-t-md border-gold"
+                    onClick={handleContentClick}
+                >
+                    <h1 className="text-lg text-center">Ucapan dan Doa</h1>
+
+                    <button
+                        className="absolute top-1/4 right-[1rem]"
+                        onClick={() => setOpen(false)}
+                    >
+                        x
+                    </button>
                 </div>
 
-                <div className='w-full p-4 pt-3 bg-white border-t border-gold rounded-b-md md:p-6 md:pt-3' onClick={handleContentClick}>
-                    <form className='flex flex-col w-full text-blue-floral' onSubmit={handleSubmitGuestBook}>
-                        <span className='text-[11px] md:text-xs text-blue-floral'>
-                            Perhatian! Ucapan yang telah dikirim tidak dapat dihapus.
+                <div
+                    className="w-full p-4 pt-3 bg-white border-t border-gold rounded-b-md md:p-6 md:pt-3"
+                    onClick={handleContentClick}
+                >
+                    <form
+                        className="flex flex-col w-full text-blue-floral"
+                        onSubmit={handleSubmitGuestBook}
+                    >
+                        <span className="text-[11px] md:text-xs text-blue-floral">
+                            Perhatian! Ucapan yang telah dikirim tidak dapat
+                            dihapus.
                         </span>
 
-                        <input type='text'
+                        <input
+                            type="text"
                             value={fullname}
-                            className='w-full px-2 py-1 mt-2 text-sm bg-gray-100 border rounded border-gold'
-                            placeholder='Nama (beserta alamat singkat juga boleh 🤗)'
+                            className="w-full px-2 py-1 mt-2 text-sm bg-gray-100 border rounded border-gold"
+                            placeholder="Nama (beserta alamat singkat juga boleh 🤗)"
                             disabled={submitting}
                             required={true}
                             onChange={e => setFullname(e.target.value)}
                         />
 
-                        <textarea value={message}
-                            className='w-full px-2 py-1 mt-2 text-sm bg-gray-100 border rounded border-gold'
+                        <textarea
+                            value={message}
+                            className="w-full px-2 py-1 mt-2 text-sm bg-gray-100 border rounded border-gold"
                             maxLength={200}
                             rows={5}
-                            placeholder='Ucapan dan doa (maksimal 200 huruf ya 😉)'
+                            placeholder="Ucapan dan doa (maksimal 200 huruf ya 😉)"
                             disabled={submitting}
                             required={true}
                             onChange={e => setMessage(e.target.value)}
                         />
 
-                        <select className='w-full px-2 py-1 mt-2 text-sm bg-gray-100 border rounded border-gold'
+                        <select
+                            className="w-full px-2 py-1 mt-2 text-sm bg-gray-100 border rounded border-gold"
                             value={presence}
                             disabled={submitting}
                             required={true}
-                            onChange={(e) => setPresence(parseInt(e.target.value))}
+                            onChange={e =>
+                                setPresence(parseInt(e.target.value))
+                            }
                         >
-                            <option value={0} disabled={true} >Konfirmasi Kehadiran</option>
+                            <option
+                                value={0}
+                                disabled={true}
+                            >
+                                Konfirmasi Kehadiran
+                            </option>
                             <option value={1}>Akan Hadir</option>
                             <option value={2}>Tidak Hadir</option>
                             <option value={3}>Telah Hadir</option>
                         </select>
 
-                        <div className='flex flex-row flex-wrap items-center w-full mt-2'>
-                            <button type='submit' className='w-full px-3 py-1 text-sm rounded-2xl bg-gold text-blue-floral hover:bg-gold2'>
+                        <div className="flex flex-row flex-wrap items-center w-full mt-2">
+                            <button
+                                type="submit"
+                                className="w-full px-3 py-1 text-sm rounded-2xl bg-gold text-blue-floral hover:bg-gold2"
+                            >
                                 {submitting ? 'Mengirim...' : 'Kirim'}
                             </button>
                         </div>
