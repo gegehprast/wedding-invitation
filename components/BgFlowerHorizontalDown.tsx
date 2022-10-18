07cfc7bb86e4bd45f48dd729b4f48fe0bd2d740a
gegehprast <gegeh.prast@gmail.com>
2022-10-18 21:05:42 +0700

    only use flower-horizontal-up

--- components/BgFlowerHorizontalDown.tsx
+++ components/BgFlowerHorizontalDown.tsx
@@ -7,7 +7,7 @@ interface Props {
 const BgFlowerHorizontalDown: React.FC<Props> = ({ className }) => {
     return (
         <div className={`absolute w-full h-full ${className}`}>
-            <div className={`w-full h-full bg-no-repeat bg-contain bg-[url(/svg/flower-horizontal-down.png)] bg-[position:center_0]`}>
+            <div className={`w-full h-full bg-no-repeat bg-contain bg-[url(/svg/flower-horizontal-up.png)] bg-[position:center_bottom] rotate-180`}>
 
             </div>
         </div>
